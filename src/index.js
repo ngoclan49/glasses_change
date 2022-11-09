eea02ad8f60e7a34b66282f9da25af44e359c732
ngoclan49 <ngocdiepdn213@gmail.com>
2022-11-09 11:07:53 +0700

    update bt changeglasses

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ChooseGlasses from './components/ChooseGlasses';
+import './index.scss';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <ChooseGlasses />
+);
+
